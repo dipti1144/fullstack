09eac437b5f5274319d618daccb7f31e810a850a
deepti1144 <dipz09085@gmail.com>
2023-03-25 22:05:57 +0530

    fullstack-app

--- client/front-end/src/Components/Movies.jsx
+++ client/front-end/src/Components/Movies.jsx
@@ -1,12 +1,13 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from "axios"
 
 const Movies = () => {
+  const [data,setData]=useState([])
 
   const getmoviedata=async()=>{
-    const res= await axios.get(`http://localhost:8080`);
-    const data=await res.json()
-    console.log(data)
+    const res= await axios.get(`https://repulsive-pink-mackerel.cyclic.app/movie`);
+     setData(res.data)
+     console.log(res.data)
   }
 
  useEffect(()=>{
@@ -14,7 +15,18 @@ const Movies = () => {
  },[])
 
   return (
-    <div>Movies</div>
+    <div>
+     {
+      data.map((el)=>(
+        <div key={el._id} style={{border:"1px solid black"}} >
+          <h1>{el.movie_name}</h1>
+          <h2>{el.genre}</h2>
+          <h3>{el.rating}</h3>
+          <p>{el.director}</p>
+        </div>
+      ))
+     }
+    </div>
   )
 }
 
--- server/config/db.js
+++ server/config/db.js
@@ -1,18 +1,13 @@
-
 const mongoose = require("mongoose");
 // mongoose.set("strictQuery", false);
 const connection = () => {
-
   mongoose
     .connect(process.env.MONGOOSE_URL, {
       useNewUrlParser: true,
-
     })
     .then((data) => {
       console.log(`Mongodb connected with server: ${data.connection.host}`);
-    })
-
-   
+    });
 };
 
-module.exports = {connection};
+module.exports = { connection };
